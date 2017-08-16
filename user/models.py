8ec8106cd06d80d930cff6c7eeef521ffc61e629
ross <820584458@qq.com>
2017-08-16 10:59:13 +0800

    clear useless import

--- user/models.py
+++ user/models.py
@@ -1,8 +1,6 @@
 # coding: utf-8
 from django.db import models
-from django.contrib.auth.models import AbstractUser, \
-    AbstractBaseUser, PermissionsMixin, BaseUserManager, \
-    UserManager, User
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.utils.translation import ugettext_lazy as _
 
 
