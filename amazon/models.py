3a276117a93b3b25d80749fec840c51d01f718bb
ross <820584458@qq.com>
2017-08-16 22:50:21 +0800

    add keyword model

--- amazon/models.py
+++ amazon/models.py
@@ -9,13 +9,14 @@ class TopSeller(models.Model):
     user = models.ForeignKey(AmazonUser)
     title = models.CharField(max_length=256, null=False, blank=False)
     create_time = models.DateTimeField(auto_now_add=True)
-    result_file = models.FilePathField(path='file/result')
-    origin_file = models.FileField(upload_to='file/origin')
+    result_file = models.FilePathField(path='file/top/result')
+    origin_file = models.FileField(upload_to='file/top/origin')
 
 
-### 未知数据库
-# class Limit(models.Model):
-#     # user_id,keyword_limit,topseller_limit
-#     user = models.ForeignKey(AmazonUser)
-#     keyword_limit =
-#     topseller_limit =
+class Keyword(models.Model):
+    # id,title,create_time,result_file,origin_file,user_id
+    user = models.ForeignKey(AmazonUser)
+    title = models.CharField(max_length=256, null=False, blank=False)
+    create_time = models.DateTimeField(auto_now_add=True)
+    result_file = models.FilePathField(path='file/kw/result')
+    origin_file = models.FileField(upload_to='file/kw/origin')
