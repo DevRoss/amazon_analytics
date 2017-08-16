4e35c3ffc5b3b2747e11083217ca38c44d9bbe59
ross <820584458@qq.com>
2017-08-16 19:59:05 +0800

    urls optimized

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from .views import Login, Logout
 
 urlpatterns = [
-    url(r'login', Login.as_view()),
-    url(r'logout', Logout.as_view()),
+    url(r'login$', Login.as_view()),
+    url(r'logout$', Logout.as_view()),
 ]
