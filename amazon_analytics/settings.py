57015877c8c25c75ad619e5b8a75643ee403daad
ross <820584458@qq.com>
2017-08-18 13:27:30 +0800

    add error_code to exception handler

--- amazon_analytics/settings.py
+++ amazon_analytics/settings.py
@@ -142,5 +142,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 50
+    'PAGE_SIZE': 50,
+    'EXCEPTION_HANDLER': 'utils.exception_handler.custom_exception_handler'
 }
---
+++ utils/exception_handler.py
@@ -0,0 +1,72 @@
+from rest_framework.views import exception_handler
+from rest_framework import exceptions
+from django.http.response import Http404
+from django.core.exceptions import PermissionDenied
+
+"""
++-----------------------+----------------+
+|       exception       |   error_code   |
++=======================|================+
+|     APIException      |        1       |
++-----------------------+----------------+
+|                       |               |
++-----------------------+----------------+
+|                       |               |
++-----------------------+----------------+
+|                       |               |
++-----------------------+----------------+
+|                       |               |
++-----------------------+----------------+
+|                       |               |
++-----------------------+----------------+
+|                       |               |
++-----------------------+----------------+
+|                       |               |
++-----------------------+----------------+
+
+"""
+
+
+def custom_exception_handler(exc, context):
+    # Call REST framework's default exception handler first,
+    # to get the standard error response.
+    response = exception_handler(exc, context)
+
+    # 扩展原来的exception, 添加error_code
+    if isinstance(exc, exceptions.APIException):
+        response.data['error_code'] = 1
+
+    elif isinstance(exc, Http404):
+        response.data['error_code'] = 2
+
+    elif isinstance(exc, exceptions.NotFound):
+        response.data['error_code'] = 2
+
+    elif isinstance(exc, exceptions.MethodNotAllowed):
+        response.data['error_code'] = 3
+
+    elif isinstance(exc, exceptions.NotAcceptable):
+        response.data['error_code'] = 4
+
+    elif isinstance(exc, exceptions.Throttled):
+        response.data['error_code'] = 5
+
+    elif isinstance(exc, exceptions.ParseError):
+        response.data['error_code'] = 10
+
+    elif isinstance(exc, exceptions.ValidationError):
+        response.data['error_code'] = 11
+
+    elif isinstance(exc, exceptions.UnsupportedMediaType):
+        response.data['error_code'] = 12
+
+    elif isinstance(exc, PermissionDenied):
+        response.data['error_code'] = 20
+
+    elif isinstance(exc, exceptions.AuthenticationFailed):
+        response.data['error_code'] = 21
+
+    elif isinstance(exc, exceptions.NotAuthenticated):
+        response.data['error_code'] = 22
+
+    return response
