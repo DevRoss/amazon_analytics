4c151bfe271e381d653bfc4b8e56004600af0151
ross <820584458@qq.com>
2017-08-24 11:59:21 +0800

    use asgi with redis

---
+++ amazon_analytics/routing.py
@@ -0,0 +1,6 @@
+from channels.routing import route
+channel_routing = [
+    # route("websocket.connect", ws_add),
+    # route("websocket.receive", ws_message),
+    # route("websocket.disconnect", ws_disconnect),
+]
